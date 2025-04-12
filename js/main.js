6214ed756fc6055588a5f686c86372b3a79b24da
Souichi Suto <c7druy@yahoo.co.jp>
2025-04-12 23:07:53 +0900

    reservation アクティビティ アクティビティマップ　完了 ・JQueryによるSlick実装 ・JQueryボタン押下時のマウスオーバー実装

--- js/main.js
+++ js/main.js
@@ -28,3 +28,57 @@ $(function(){
     });
    });
 });
+
+//slick
+$(function(){
+    $("#activity .slider").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 3,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+ });
+
+ //アクティビティマップ　マウスオーバー
+ $(function(){
+    $("#map .spot1 .spot-btn").mouseover(function() {
+        $("#map .spot1 .spot-img").show();
+    }).mouseout(function() {
+        $("#map .spot1 .spot-img").hide();
+    });
+
+    $("#map .spot2 .spot-btn").mouseover(function() {
+        $("#map .spot2 .spot-img").show();
+    }).mouseout(function() {
+        $("#map .spot2 .spot-img").hide();
+    });
+
+    $("#map .spot3 .spot-btn").mouseover(function() {
+        $("#map .spot3 .spot-img").show();
+    }).mouseout(function() {
+        $("#map .spot3 .spot-img").hide();
+    });
+
+    $("#map .spot4 .spot-btn").mouseover(function() {
+        $("#map .spot4 .spot-img").show();
+    }).mouseout(function() {
+        $("#map .spot4 .spot-img").hide();
+    });
+
+    $("#map .spot5 .spot-btn").mouseover(function() {
+        $("#map .spot5 .spot-img").show();
+    }).mouseout(function() {
+        $("#map .spot5 .spot-img").hide();
+    });
+ });
